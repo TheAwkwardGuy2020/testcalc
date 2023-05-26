242578c87e38d4c785e398df63334fc44d072bc4
Awx <rishavghosh2007@gmail.com>
2023-05-26 07:54:55 +0530

    V:1.0 initialized repo and added files

---
+++ README.md
@@ -0,0 +1,1 @@
+#This is simply a test repo for me/us to learn GitHub and Git
---
+++ subtraction.js
@@ -0,0 +1,7 @@
+function subtract(a, b){
+    return a - b;
+    }
+    
+    console.log(subtract());
+
+//this is the changed file, maybe
